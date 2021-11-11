15c3c49f92fb211579196c10165bcfef428634a0
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2021-11-11 19:48:18 +0200

    Remove previous remix

--- website_old/src/components/List.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-const List = styled.ul`
-  list-style: none;
-  margin: unset;
-  padding: unset;
-`;
-
-export default List;
--- website_old/src/database/index.ts
+++
@@ -1,9 +0,0 @@
-import welcome from "./welcome";
-import career from "./career";
-import learning from "./learning";
-import portfolio from "./portfolio";
-import contact from "./contact";
-
-const mainPages = [welcome, career, learning, portfolio, contact];
-
-export default mainPages;
