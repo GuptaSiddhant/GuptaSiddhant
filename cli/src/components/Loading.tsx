e1030c913a547142a38a57c21f285e22756675d0
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2021-11-28 22:40:31 +0200

    Remove cli and website

--- cli/src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Newline, Text } from "ink";
-
-export default function LoadingText(): JSX.Element {
-  return (
-    <Text dimColor>
-      {"Loading..."} <Newline />
-    </Text>
-  );
-}
--- cli/src/helpers/constants.ts
+++
@@ -1,1 +0,0 @@
-export const PADDING_X = 2;
