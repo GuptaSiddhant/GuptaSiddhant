ea777aa6e10c76b954fa38f31d82e33b73f31681
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-05-30 01:39:45 +0300

    Add remix website - deno

---
+++ apps/website/app/routes/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>GuptaSiddhant</h1>
+      <h1>Deno deploy</h1>
+    </div>
+  )
+}
