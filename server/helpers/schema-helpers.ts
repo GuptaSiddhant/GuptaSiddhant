e57d445f51180c95ad388df4d0fa2f2be6273b35
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-05-29 17:09:26 +0300

    git init

--- server/helpers/schema-helpers.ts
+++
@@ -1,9 +0,0 @@
-import type { Preview } from "./schema-type";
-
-export function createDocumentPreview(
-  title: string,
-  subtitle: string,
-  media: string = ""
-): Preview {
-  return { select: { title, subtitle, media } };
-}
