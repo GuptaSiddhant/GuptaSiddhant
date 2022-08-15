9be0427a8f13b4ea8c984ebf19176c1f4a9f4359
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-08-15 17:47:57 +0300

    Extract recanvas as standalone package

--- packages/recanvas/src/dom/constants.ts
+++
@@ -1,9 +0,0 @@
-export enum ElementName {
-  Root = "canvas-root",
-  View = "canvas-view",
-  Text = "canvas-text",
-  VirtualText = "canvas-virtual-text",
-}
-
-export const TEXT_NAME = "#text"
-export type TextName = typeof TEXT_NAME
