04f18e5ad840467efafafd5763ae02ac3e1b99b5
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-08-16 13:45:34 +0300

    Add imager firebase

--- web/entry.server.tsx
+++ web/entry.server.tsx
@@ -1,16 +1,17 @@
-import { renderToString } from "react-dom/server"
+import { renderToPipeableStream } from "react-dom/server"
+import { PassThrough } from "stream"
 
+import { Response } from "@remix-run/node"
 import { RemixServer } from "@remix-run/react"
 import type { EntryContext } from "@remix-run/server-runtime"
 
 import { initFirebase } from "@gs/firebase/init"
-import { setupVercel } from "@gs/service/vercel.server"
 
 global.__IS_SERVER__ = typeof window === "undefined"
 global.__IS_DEV__ = process.env.NODE_ENV === "development"
+const ABORT_DELAY = 5_000
 
 initFirebase()
-setupVercel()
 
 export default function handleRequest(
   request: Request,
@@ -18,16 +19,38 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />,
-  )
+  return new Promise((resolve, reject) => {
+    let didError = false
 
-  responseHeaders.set("Content-Type", "text/html")
-  responseHeaders.set("X-Powered-By", "Remix-Run")
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        onShellReady: () => {
+          const body = new PassThrough()
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+          responseHeaders.set("Content-Type", "text/html")
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            }),
+          )
+
+          pipe(body)
+        },
+        onShellError: (err) => {
+          reject(err)
+        },
+        onError: (error) => {
+          didError = true
+
+          console.error(error)
+        },
+      },
+    )
+
+    setTimeout(abort, ABORT_DELAY)
   })
 }
 
