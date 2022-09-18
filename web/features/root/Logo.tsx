c5b33f0bf815bd0f96c9a891db972d1879302bb3
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-09-18 17:48:33 +0300

    update logo

--- web/features/root/Logo.tsx
+++ web/features/root/Logo.tsx
@@ -1,20 +1,13 @@
 import { useRef } from "react"
 
-import { Link, useLoaderData, useNavigate } from "@remix-run/react"
+import { Link, useNavigate } from "@remix-run/react"
 
 import useLongPress from "@gs/hooks/useLongPress"
-
-import type { RootLoaderData } from "."
 
 export default function Logo(): JSX.Element | null {
   const ref = useRef<HTMLAnchorElement>(null)
   const navigate = useNavigate()
-  const data = useLoaderData<RootLoaderData | undefined>()
-
   useLongPress(ref, () => navigate("/admin"))
-
-  const name = data?.about.name || "Siddhant Gupta"
-  const shortName = data?.about.shortName || "GS"
 
   return (
     <Link
@@ -28,14 +21,14 @@ export default function Logo(): JSX.Element | null {
           "hidden text-xl font-black uppercase leading-normal tracking-widest text-primary sm:block"
         }
       >
-        {name}
+        Gupta Siddhant
       </span>
       <span
         className={
           "text-3xl font-black uppercase leading-none tracking-widest text-primary sm:hidden"
         }
       >
-        {shortName}
+        GS
       </span>
     </Link>
   )
