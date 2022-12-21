c56691b528212c964c6ba50073d0668c3c34fbe8
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-12-21 23:25:31 +0200

    Add server response headers

--- web/entry.client.tsx
+++ web/entry.client.tsx
@@ -1,19 +1,29 @@
-import { StrictMode } from "react"
-import { hydrateRoot } from "react-dom/client"
+import { StrictMode, startTransition } from "react";
+import { hydrateRoot } from "react-dom/client";
 
-import { RemixBrowser } from "@remix-run/react"
+import { RemixBrowser } from "@remix-run/react";
 
-global.__IS_SERVER__ = typeof window === "undefined"
-global.__IS_DEV__ = process.env.NODE_ENV === "development"
+global.__IS_SERVER__ = typeof window === "undefined";
+global.__IS_DEV__ = process.env.NODE_ENV === "development";
 
-hydrateRoot(
-  document,
-  <StrictMode>
-    <RemixBrowser />
-  </StrictMode>,
-)
+function hydrate() {
+	startTransition(() => {
+		hydrateRoot(
+			document,
+			<StrictMode>
+				<RemixBrowser />
+			</StrictMode>,
+		);
+	});
+}
+
+if (window.requestIdleCallback) {
+	window.requestIdleCallback(hydrate);
+} else {
+	window.setTimeout(hydrate, 1);
+}
 
 declare global {
-  var __IS_SERVER__: boolean
-  var __IS_DEV__: boolean
+	var __IS_SERVER__: boolean;
+	var __IS_DEV__: boolean;
 }
--- web/entry.server.tsx
+++ web/entry.server.tsx
@@ -7,6 +7,7 @@ import type { EntryContext } from "@remix-run/server-runtime";
 
 import { initFirebase } from "@gs/firebase/init";
 import { appLogger } from "@gs/service/logger.server";
+import { getErrorMessage } from "@gs/utils/error";
 
 global.__IS_SERVER__ = typeof window === "undefined";
 global.__IS_DEV__ = process.env.NODE_ENV === "development";
@@ -15,49 +16,57 @@ const ABORT_DELAY = 5_000;
 initFirebase();
 
 export default function handleRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext,
+	request: Request,
+	responseStatusCode: number,
+	responseHeaders: Headers,
+	remixContext: EntryContext,
 ) {
-  appLogger.logRequest(request);
+	appLogger.logRequest(request);
 
-  return new Promise((resolve, reject) => {
-    let didError = false;
+	return new Promise((resolve, reject) => {
+		let didError = false;
 
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        onShellReady: () => {
-          const body = new PassThrough();
+		const { pipe, abort } = renderToPipeableStream(
+			<RemixServer context={remixContext} url={request.url} />,
+			{
+				onShellReady: () => {
+					const body = new PassThrough();
 
-          responseHeaders.set("Content-Type", "text/html");
+					responseHeaders.set("Content-Type", "text/html; charset=utf-8");
+					responseHeaders.set("x-powered-by", "Remix.run");
 
-          resolve(
-            new Response(body, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            }),
-          );
+					if (process.env.NODE_ENV !== "production") {
+						responseHeaders.set("Cache-Control", "no-store");
+					} else {
+						responseHeaders.set("Cache-Control", "3600");
+						responseHeaders.set("stale-while-revalidate", "86400");
+					}
 
-          pipe(body);
-        },
-        onShellError: (err) => {
-          reject(err);
-        },
-        onError: (error) => {
-          didError = true;
+					resolve(
+						new Response(body, {
+							headers: responseHeaders,
+							status: didError ? 500 : responseStatusCode,
+						}),
+					);
 
-          console.error(error);
-        },
-      },
-    );
+					pipe(body);
+				},
 
-    setTimeout(abort, ABORT_DELAY);
-  });
+				onShellError: reject,
+
+				onError: (error) => {
+					didError = true;
+
+					appLogger.error(getErrorMessage(error));
+				},
+			},
+		);
+
+		setTimeout(abort, ABORT_DELAY);
+	});
 }
 
 declare global {
-  var __IS_SERVER__: boolean;
-  var __IS_DEV__: boolean;
+	var __IS_SERVER__: boolean;
+	var __IS_DEV__: boolean;
 }
