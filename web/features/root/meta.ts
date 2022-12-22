d60835e3a3dd13a459615e99de4612d48f71f89b
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2022-12-22 05:14:36 +0200

    Update meta to only block scale in pwa

--- web/features/root/meta.ts
+++ web/features/root/meta.ts
@@ -1,41 +1,53 @@
-import { type MetaDescriptor } from "@remix-run/server-runtime"
+import { type MetaDescriptor } from "@remix-run/server-runtime";
 
-import { getThemeFromThemeName } from "@gs/theme"
+import { getThemeFromThemeName } from "@gs/theme";
 
-import type { RootLoaderData } from "."
+import type { RootLoaderData } from ".";
 
 export default function meta({
-  data,
+	data,
+	location,
 }: {
-  data: RootLoaderData
+	data: RootLoaderData;
+	location: Location;
 }): MetaDescriptor {
-  const { themeName, about } = data
-  const { name = "Siddhant Gupta", shortName = "GS" } = about
-  const description = "Webfolio of a creator."
+	const { themeName, about } = data;
+	const { name = "Siddhant Gupta", shortName = "GS" } = about;
+	const description = "Webfolio of a creator.";
 
-  const theme = getThemeFromThemeName(themeName)
-  const themeColor = theme.bg.default
+	const isPwa = new URLSearchParams(location.search).has("pwa");
+	const theme = getThemeFromThemeName(themeName);
+	const themeColor = theme.bg.default;
+	const viewport = [
+		"width=device-width",
+		"initial-scale=1.0",
+		"viewport-fit=cover",
+		isPwa ? "maximum-scale=1.0" : "",
+	]
+		.filter(Boolean)
+		.join(",");
 
-  return {
-    title: name,
-    description,
-    charset: "utf-8",
-    viewport:
-      "width=device-width,initial-scale=1.0,maximum-scale=1.0,viewport-fit=cover",
+	return {
+		title: name,
+		description,
+		charset: "utf-8",
+		viewport,
 
-    "application-name": shortName,
-    "apple-mobile-web-app-title": shortName,
-    "theme-color": themeColor,
-    "msapplication-TileColor": themeColor,
-    "msapplication-config": "/assets/browserconfig.xml",
-    // Open graph
-    "og:title": name,
-    "og:description": description,
-    "og:locale": "en_GB",
-    "og:type": "website",
-    "og:site_name": shortName,
-    // Twitter
-    "twitter:creator": "@guptasiddhant9",
-    "twitter:card": "summary",
-  }
+		"application-name": shortName,
+		"apple-mobile-web-app-title": shortName,
+		"theme-color": themeColor,
+		"msapplication-TileColor": themeColor,
+		"msapplication-config": "/assets/browserconfig.xml",
+
+		// Open graph
+		"og:title": name,
+		"og:description": description,
+		"og:locale": "en_GB",
+		"og:type": "website",
+		"og:site_name": shortName,
+
+		// Twitter
+		"twitter:creator": "@guptasiddhant9",
+		"twitter:card": "summary",
+	};
 }
--- web/routes/__assets/site[.webmanifest].ts
+++ web/routes/__assets/site[.webmanifest].ts
@@ -1,57 +1,65 @@
-import type { LoaderArgs } from "@remix-run/server-runtime"
+import type { LoaderArgs } from "@remix-run/server-runtime";
 
-import { getAboutInfo } from "@gs/models/about/index.server"
-import { internalNavigationLinks } from "@gs/navigation/useNavigationLinks"
-import { DEFAULT_THEME, getThemeFromThemeName } from "@gs/theme"
-import type { WebApplicationManifest } from "@gs/types/webmanifest"
+import { ONE_DAY_IN_S } from "@gs/constants";
+import { getAboutInfo } from "@gs/models/about/index.server";
+import { internalNavigationLinks } from "@gs/navigation/useNavigationLinks";
+import { DEFAULT_THEME, getThemeFromThemeName } from "@gs/theme";
+import type { WebApplicationManifest } from "@gs/types/webmanifest";
 
-export async function loader(_: LoaderArgs): Promise<Response> {
-  const [about] = await Promise.all([getAboutInfo()])
+export async function loader({ request }: LoaderArgs): Promise<Response> {
+	const [about] = await Promise.all([getAboutInfo()]);
 
-  const theme = getThemeFromThemeName(DEFAULT_THEME)
-  const themeColor = theme.bg.default
+	const origin = new URL(request.url).origin;
+	const start_url = `${origin}?pwa`;
 
-  const manifest: WebApplicationManifest = {
-    name: about.name,
-    short_name: about.shortName,
-    description: about.title,
-    start_url: ".",
-    scope: ".",
-    theme_color: themeColor,
-    background_color: themeColor,
-    display: "standalone",
-    dir: "ltr",
-    lang: "en-GB",
-    orientation: "portrait-primary",
-    icons: [
-      {
-        src: "/assets/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/assets/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-    shortcuts: [
-      {
-        name: "Admin",
-        url: "/admin",
-        description: "GS Admin",
-      },
-      ...internalNavigationLinks.map((link) => ({
-        name: link.children?.toString() || link.id,
-        url: link.to?.toString() || "/",
-      })),
-    ],
-  }
+	const theme = getThemeFromThemeName(DEFAULT_THEME);
+	const themeColor = theme.bg.default;
 
-  return new Response(JSON.stringify(manifest, null, 2), {
-    status: 200,
-    headers: { "Content-Type": "manifest/json" },
-  })
+	const manifest: WebApplicationManifest = {
+		id: "com.guptasiddhant.web",
+		name: about.name,
+		short_name: about.shortName,
+		description: about.title,
+		start_url,
+		scope: ".",
+		theme_color: themeColor,
+		background_color: themeColor,
+		display: "standalone",
+		dir: "ltr",
+		lang: "en-GB",
+		orientation: "portrait-primary",
+		icons: [
+			{
+				src: "/assets/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/assets/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+		],
+		shortcuts: [
+			{
+				name: "Admin",
+				url: "/admin",
+				description: "GS Admin",
+			},
+			...internalNavigationLinks.map((link) => ({
+				name: link.children?.toString() || link.id,
+				url: link.to?.toString() || "/",
+			})),
+		],
+	};
+
+	return new Response(JSON.stringify(manifest, null, 2), {
+		status: 200,
+		headers: {
+			"Content-Type": "manifest/json",
+			"Cache-Control": ONE_DAY_IN_S.toString(),
+		},
+	});
 }
 
 export function CatchBoundary() {}
