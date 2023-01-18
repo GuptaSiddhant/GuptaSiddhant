5871160ee16bcd31c2f07dd9a691a0107c29898e
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2023-01-18 22:13:13 +0200

    remix: Add blog rss feed

--- apps/remix/src/features/models/blog/index.server.ts
+++ apps/remix/src/features/models/blog/index.server.ts
@@ -1,12 +1,19 @@
 import { ModelName } from "@gs/models";
 import Database from "@gs/service/database.server";
-import { type SummaryItem, getCrossSellSummaryItems } from "@gs/summary";
+import {
+  type SummaryItem,
+  getCrossSellSummaryItems,
+  sortSummaryItemsByDateLatestFirstPredicate,
+} from "@gs/summary";
 import { querySummaryItemsByModelName } from "@gs/summary/service.server";
+import { generateRss } from "@gs/utils/rss";
 
 import type { BlogPostProps } from ".";
 
 const modelName = ModelName.Blog;
 const db = new Database<BlogPostProps>(modelName);
+
+export { BlogPostProps };
 
 export function getBlogModelName() {
   return modelName;
@@ -40,4 +47,24 @@ export async function getBlogPostCrossSell(
   return getCrossSellSummaryItems(items, id).slice(0, limit);
 }
 
-export { BlogPostProps };
+export async function generateBlogFeed(origin: string) {
+  const posts = await getBlogSummaryItems();
+  const baseUrl = `${origin}/blog`;
+
+  return generateRss({
+    title: "GS Blog",
+    description: "Thoughts on somethings. Sometimes everything.",
+    link: baseUrl,
+    origin,
+    entries: posts
+      .sort(sortSummaryItemsByDateLatestFirstPredicate)
+      .map((post) => ({
+        description: post.description || post.subtitle || "",
+        pubDate: post.date ? new Date(post.date) : new Date(),
+        title: post.title,
+        link: `${baseUrl}/${post.id}`,
+        guid: `${baseUrl}/${post.id}`,
+        author: "Siddhant Gupta",
+      })),
+  });
+}
