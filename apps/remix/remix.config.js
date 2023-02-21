5f10d15b678ba03d0863a7538fb7c92cd1d53174
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2023-02-21 06:54:44 +0530

    remix: Update vercel

--- apps/remix/remix.config.js
+++ apps/remix/remix.config.js
@@ -1,8 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "vercel",
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: [".*"],
+  serverBuildPath: "api/index.js",
+  ignoredRouteFiles: ["**/.*"],
   appDirectory: "src",
   future: {
     v2_routeConvention: true,
