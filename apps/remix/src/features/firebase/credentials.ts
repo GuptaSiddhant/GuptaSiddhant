ac2348aca4b64c39da0151bb7e55f50c4261aea7
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2023-02-24 09:55:33 +0200

    remix: Update google credential typing

--- apps/remix/src/features/firebase/credentials.ts
+++ apps/remix/src/features/firebase/credentials.ts
@@ -1,17 +1,16 @@
+import type { ServiceAccount } from "firebase-admin/lib/app/credential";
 import { z } from "zod";
 
 import invariant from "@gs/utils/invariant";
 
-export function googleServiceAccount() {
+export function googleServiceAccount(): ServiceAccount {
   const FIREBASE_SERVICE_ACCOUNT_KEY = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
   invariant(
     FIREBASE_SERVICE_ACCOUNT_KEY,
     "Env FIREBASE_SERVICE_ACCOUNT_KEY is required.",
   );
 
-  const serviceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY);
-
-  return z
+  const serviceAccount = z
     .object({
       type: z.string(),
       project_id: z.string(),
@@ -24,5 +23,12 @@ export function googleServiceAccount() {
       auth_provider_x509_cert_url: z.string(),
       client_x509_cert_url: z.string(),
     })
-    .parse(serviceAccount);
+    .parse(JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY));
+
+  return {
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key,
+    projectId: serviceAccount.project_id,
+    ...serviceAccount,
+  };
 }
