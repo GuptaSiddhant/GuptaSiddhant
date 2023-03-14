df0e8affcd4f51af90c22bc5f9513d9b0497dbdc
Siddhant Gupta <siddhant.c.gupta@accenture.com>
2023-03-14 21:10:37 +0200

    remix: add tests for error-utils

--- apps/remix/src/features/utils/error.ts
+++ apps/remix/src/features/utils/error.ts
@@ -1,13 +1,13 @@
-interface ErrorWithMessage {
+export interface ErrorWithMessage {
   message: string;
 }
 
 export function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
-  return (
+  return Boolean(
     typeof error === "object" &&
-    error !== null &&
-    "message" in error &&
-    typeof (error as Record<string, unknown>).message === "string"
+      error !== null &&
+      "message" in error &&
+      typeof (error as Record<string, unknown>).message === "string",
   );
 }
 
@@ -17,12 +17,12 @@ export function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
   try {
     return new Error(JSON.stringify(maybeError));
   } catch {
-    // fallback in case there's an error stringifying the maybeError
+    // fallback in case there's an error stringify-ing the maybeError
     // like with circular references for example.
     return new Error(String(maybeError));
   }
 }
 
-export function getErrorMessage(error: unknown) {
+export function getErrorMessage(error: unknown): string {
   return toErrorWithMessage(error).message;
 }
