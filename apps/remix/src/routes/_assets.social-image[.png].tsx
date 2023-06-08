53472378afa4ad5318e30bc96d8e60aff38ee1d6
Siddhant Gupta <guptasiddhant@icloud.com>
2023-06-08 17:03:39 +0300

    remix: update og image next url

--- apps/remix/src/routes/_assets.social-image[.png].tsx
+++ apps/remix/src/routes/_assets.social-image[.png].tsx
@@ -40,7 +40,7 @@ export async function loader({ request }: DataFunctionArgs) {
   });
 
   const image = await fetch(
-    `https://next.guptasiddhant.com/api/og?${params.toString()}`,
+    `https://next.guptasiddhant.com/og?${params.toString()}`,
   ).then((res) => res.blob());
 
   return new Response(image, {
